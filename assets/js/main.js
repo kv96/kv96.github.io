6709559f7f87607020fefb6380fb35f9090bb128
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-04-27 20:51:47 +0000

    Deploy k17n/Portfolio to github.com/k17n/Portfolio.git:gh-pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,69 +1,127 @@
+const HIGHTLIGHT_JS_REQUIRED = $("body").data("highlightjs-required");
+const HIGHLIGHT_JS_LINKS = {
+  js: "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.7.2/highlight.min.js",
+  css_light: "https://cdn.jsdelivr.net/npm/highlight.js@10.7.2/styles/github.css",
+  css_dark: "https://cdn.jsdelivr.net/npm/highlight.js@10.7.2/styles/vs2015.css",
+};
+
 ////// THEME ///////
 // Set theme on page load
 $(document).ready(function () {
-    initTheme(); // on page load, if user has already selected a specific theme -> apply it
+  initTheme(); // on page load, if user has already selected a specific theme -> apply it
+  if (HIGHTLIGHT_JS_REQUIRED) {
+    loadHighlightJsScript();
+  }
 });
 
 // Check local storage for any theme attribute and apply it
 function initTheme() {
-    var darkThemeSelected = (localStorage.getItem('theme') !== null && localStorage.getItem('theme') === 'dark');
+  var darkThemeSelected =
+    localStorage.getItem("theme") !== null &&
+    localStorage.getItem("theme") === "dark";
 
-    // update body data-theme attribute
-    if (darkThemeSelected) {
-        document.body.setAttribute('data-theme', 'dark');
-        $('#darkThemeSwitch').addClass('active');
-        $('.icon-dark.theme-switcher__btn').hide();
-        $("meta[name='theme-color']").attr('content', '#222222');
+  // update body data-theme attribute
+  if (darkThemeSelected) {
+    document.body.setAttribute("data-theme", "dark");
+    $("#darkThemeSwitch").addClass("active");
+    $(".icon-dark.theme-switcher__btn").hide();
+    $("meta[name='theme-color']").attr("content", "#222222");
+    if (HIGHTLIGHT_JS_REQUIRED) {
+      toggleHighlightJsTheme("dark");
     }
-    else {
-        document.body.removeAttribute('data-theme');
-        $('#lightThemeSwitch').addClass('active');
-        $('.icon-sun.theme-switcher__btn').hide();
-        $("meta[name='theme-color']").attr('content', '#f6f6f6');
+  } else {
+    document.body.removeAttribute("data-theme");
+    $("#lightThemeSwitch").addClass("active");
+    $(".icon-sun.theme-switcher__btn").hide();
+    $("meta[name='theme-color']").attr("content", "#f6f6f6");
+    if (HIGHTLIGHT_JS_REQUIRED) {
+      toggleHighlightJsTheme();
     }
-};
+  }
+}
 
-// Update theme and set the local storage attribute and browser theme color 
-$('.theme-switcher__label').click(function () {
-    var themeOption = $(this).data("theme-option");
-    $('.theme-switcher__label').removeClass('active');
-    $('.theme-switcher__btn').hide();
-    if (themeOption === 'dark') {
-        document.body.setAttribute('data-theme', 'dark');
-        $(this).addClass('active');
-        $('.theme-switcher__btn.icon-sun').show();
-        $("meta[name='theme-color']").attr('content', '#222222');
-        localStorage.setItem('theme', 'dark'); // save theme selection 
+function loadHighlightJsScript() {
+  $.getScript(HIGHLIGHT_JS_LINKS.js, function () {
+    $("[data-code-file-path]").each(function (i) {
+      let $this = $(this);
+      let $filePath = $this.data("code-file-path");
+      let $fileType = $this.data("code-lang");
+      $.get($filePath, function (data) {
+        $this.text(data);
+        $this.addClass($fileType);
+        hljs.highlightElement($this.get(0));
+      });
+    });
+  });
+}
+
+function toggleHighlightJsTheme(theme) {
+  // Remove existing Hightlight.js theme CSS
+  $(".highlightjs-theme").remove();
+
+  // Append the CSS file based on the selected Theme
+  if (theme === "dark") {
+    $("head").append(
+      `<link class="highlightjs-theme" rel="stylesheet" href="${HIGHLIGHT_JS_LINKS.css_dark}">`
+    );
+  } else {
+    $("head").append(
+      `<link class="highlightjs-theme" rel="stylesheet" href="${HIGHLIGHT_JS_LINKS.css_light}">`
+    );
+  }
+}
+
+// Update theme and set the local storage attribute and browser theme color
+$(".theme-switcher__label").click(function () {
+  var themeOption = $(this).data("theme-option");
+  $(".theme-switcher__label").removeClass("active");
+  $(".theme-switcher__btn").hide();
+  if (themeOption === "dark") {
+    document.body.setAttribute("data-theme", "dark");
+    $(this).addClass("active");
+    $(".theme-switcher__btn.icon-sun").show();
+    $("meta[name='theme-color']").attr("content", "#222222");
+    if (HIGHTLIGHT_JS_REQUIRED) {
+      toggleHighlightJsTheme("dark");
     }
-    else {
-        document.body.removeAttribute('data-theme');
-        $(this).addClass('active');
-        $('.theme-switcher__btn.icon-dark').show();
-        $("meta[name='theme-color']").attr('content', '#f6f6f6');
-        localStorage.removeItem('theme'); // reset theme selection 
+    localStorage.setItem("theme", "dark"); // save theme selection
+  } else {
+    document.body.removeAttribute("data-theme");
+    $(this).addClass("active");
+    $(".theme-switcher__btn.icon-dark").show();
+    $("meta[name='theme-color']").attr("content", "#f6f6f6");
+    if (HIGHTLIGHT_JS_REQUIRED) {
+      toggleHighlightJsTheme();
     }
+    localStorage.removeItem("theme"); // reset theme selection
+  }
 });
 
-$('.theme-switcher__btn').click(function () {
-    var themeOption = $(this).data("theme-option");
-    $('.theme-switcher__label').removeClass('active');
-    $('.theme-switcher__btn').hide();
-    if (themeOption === 'dark') {
-        document.body.setAttribute('data-theme', 'dark');
-        $('#darkThemeSwitch').addClass('active');
-        $('.theme-switcher__btn.icon-sun').show();
-        $("meta[name='theme-color']").attr('content', '#222222');
-        localStorage.setItem('theme', 'dark'); // save theme selection 
+$(".theme-switcher__btn").click(function () {
+  var themeOption = $(this).data("theme-option");
+  $(".theme-switcher__label").removeClass("active");
+  $(".theme-switcher__btn").hide();
+  if (themeOption === "dark") {
+    document.body.setAttribute("data-theme", "dark");
+    $("#darkThemeSwitch").addClass("active");
+    $(".theme-switcher__btn.icon-sun").show();
+    $("meta[name='theme-color']").attr("content", "#222222");
+    if (HIGHTLIGHT_JS_REQUIRED) {
+      toggleHighlightJsTheme("dark");
     }
-    else {
-        document.body.removeAttribute('data-theme');
-        $('#lightThemeSwitch').addClass('active');
-        $('.theme-switcher__btn.icon-dark').show();
-        $("meta[name='theme-color']").attr('content', '#f6f6f6');
-        localStorage.removeItem('theme'); // reset theme selection 
+    localStorage.setItem("theme", "dark"); // save theme selection
+  } else {
+    document.body.removeAttribute("data-theme");
+    $("#lightThemeSwitch").addClass("active");
+    $(".theme-switcher__btn.icon-dark").show();
+    $("meta[name='theme-color']").attr("content", "#f6f6f6");
+    if (HIGHTLIGHT_JS_REQUIRED) {
+      toggleHighlightJsTheme();
     }
+    localStorage.removeItem("theme"); // reset theme selection
+  }
 });
 
 ////// COPYRIGHT YEAR ///////
 var currentYear = new Date().getFullYear();
-$('#copyrightYear').text(currentYear);
+$("#copyrightYear").text(currentYear);
